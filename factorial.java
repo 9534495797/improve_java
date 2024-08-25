1c772214d4b78578219edd190936f3ec72e82b7a
9534495797 <chaubeyankit927@gmail.com>
2024-08-25 06:41:39 +0530

    day3

---
+++ factorial.java
@@ -0,0 +1,15 @@
+package improve_java;
+import java.util.*;
+
+public class factorial {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int fact=1;
+        for(int i=num;i>=1;i--){
+fact*=i;
+
+        }
+        System.out.println(fact);
+    }
+}
