9ffd44bb9ce41a171d44a06dad9f796771bf66bb
9534495797 <chaubeyankit927@gmail.com>
2024-08-25 06:41:18 +0530

    day3

---
+++ loop.java
@@ -0,0 +1,24 @@
+package improve_java;
+
+public class loop {
+    public static void main(String[] args) {
+        
+    
+    for(int i=0;i<=5;i++){
+        System.out.println(i);
+    }
+
+    for(int i = 0; i <= 10; i++) {
+        for(int j = 0; j < i; j++) {
+            System.out.print("*");
+        }
+        System.out.println(); // Move to the next line after printing `i` asterisks
+    }
+    //while loop
+    int i=10;
+    while(i>0){
+        System.out.println("*");
+        i--;
+    }
+}
+}
